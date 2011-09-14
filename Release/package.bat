2941ede9a61a2a27dedc62bebaab77a765f98dac
TANIGAWA Takashi <mihailjp@gmail.com>
2011-09-14 13:27:00 +0900

    added batch file to create release package (using 7z.exe)

---
+++ Release/package.bat
@@ -0,0 +1,8 @@
+del /q hspbz2\*
+del /q hspbz2.zip
+rmdir hspbz2
+mkdir hspbz2
+copy hspbz2.dll hspbz2
+copy hspbz2.as hspbz2
+copy readme.txt hspbz2
+"C:\Program Files\7-Zip\7z.exe" a -mx=9 hspbz2.zip hspbz2
